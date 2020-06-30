2ff0f349d61fd6b37c747f52822fde9b629d9121
Pedro Lana <you@example.com>
2020-06-29 23:16:22 -0300

    Dando corpo ao projeto

---
+++ resources/views/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Biblioteca Virtual</title>
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <header>
+        <div class="links">
+            <a href="/"> Biblioteca Virtual </a>
+        </div>
+    </header>
+
+    <div class="main">
+        
+    </div>
+    
+</body>
+</html>
