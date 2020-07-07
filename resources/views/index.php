2ec0d73dc61d700b8d4c6ae0a572e6e139eb15f2
Pedro Lana <you@example.com>
2020-07-07 20:01:39 -0300

    Muita coisa no index.

--- resources/views/index.php
+++ resources/views/index.php
@@ -3,7 +3,9 @@
 <head>
 
     <title>Biblioteca Virtual</title>
-<link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+
 
 </head>
 <body>
@@ -14,11 +16,27 @@
     </header>
 
     <div class="main">
-        <div class="books">
-            <div class="book">
-            <img src="img/imperium" alt="">
-            <h3></h3>
+
+        <div class=" container">
+           
+            <div class="col-sm book">
+                <img src="img/imperium.jpg" alt="">
+                <h3>Imperium</h3>
+                <p>Por Robert Harris</p>
             </div>
+
+            <div class="col-sm book">
+                <img src="img/mulheres.jpg" alt="">
+                <h3>Mulheres</h3>
+                <p>Por Robert Harris</p>
+            </div>
+
+            <div class="col-sm book">
+                <img src="img/imperium.jpg" alt="">
+                <h3>Imperium</h3>
+                <p>Por Robert Harris</p>
+            </div>
+           
         </div>
     </div>
     
